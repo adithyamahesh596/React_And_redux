0783e5b34641d771cfbbb0d09728d8e1fe093194
Adithya Mahesh Bariki <adithyamaheshbariki@gmail.com>
2017-10-25 21:14:31 -0700

    Async Writes in Redux

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -4,6 +4,7 @@ import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
 
+
 // Import actions here!!
 
 class ManageCoursePage extends Component {
@@ -11,40 +12,75 @@ class ManageCoursePage extends Component {
     super(props, context);
 
     this.state = {
-      course: Object.assign({}, this.props.course),
+      course: Object.assign({}, props.course),
       errors: {}
     };
     this.updateCourseState = this.updateCourseState.bind(this);
+    this.saveCourse = this.saveCourse.bind(this);
   }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.course.id !== nextProps.course.id) {
+      this.setState({course: Object.assign({}, nextProps.course)});
+    }
+  }
+
 
   updateCourseState(event) {
     const field = event.target.name;
-    let course = Object.assign({},this.state.course);
+    let course = Object.assign({}, this.state.course);
     course[field] = event.target.value;
     return this.setState({course: course});
   }
 
+  saveCourse(event) {
+    event.preventDefault();
+    this.props.actions.saveCourse(this.state.course);
+    this.context.router.push('/courses');
+  }
+
   render() {
     return (
-      <div>
-        <CourseForm
-          allAuthors={this.props.authors}
-          onChange={this.updateCourseState}
-          course={this.state.course}
-          errors={this.state.errors}
-        />
-      </div>
+      <CourseForm
+        allAuthors={this.props.authors}
+        onChange={this.updateCourseState}
+        onSave={this.saveCourse}
+        course={this.state.course}
+        errors={this.state.errors}
+      />
+
     );
   }
 }
 
 ManageCoursePage.propTypes = {
   course: PropTypes.object.isRequired,
-  authors: PropTypes.array.isRequired
+  authors: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
 };
 
+//pulling in React router context so router is available on this.context,router.
+ManageCoursePage.contextTypes = {
+  router : PropTypes.object
+};
+
+function getCourseById(courses, id) {
+
+  const course = courses.filter(course => course.id == id);
+  if ( course.length ) {
+    return course[0];
+  }
+  return null;
+}
+
 function mapStateToProps(state, ownProps) {
+  const courseId = ownProps.params.id;
+
   let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
+
+  if( courseId && state.courses.length > 0) {
+    course = getCourseById( state.courses, courseId );
+  }
 
   const authorsFormattedForDropdown = state.authors.map(author => {
     return {
